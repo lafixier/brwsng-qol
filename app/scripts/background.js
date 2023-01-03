644d6589c2884261781804a53e79bcf4ae89c155
lafixier <lfxr@proton.me>
2023-01-03 18:26:07 +0900

    非同期を完全に理解した

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,30 @@
-setInterval(() => {
-    chrome.tabs.query({ currentWindow: true }, (tabs) => {
-        chrome.browserAction.setBadgeText({ text: String(tabs.length) });
+const StorageManager = class {
+    static getNumberOfTabs() {
+        return new Promise((resolve) => {
+            chrome.storage.local.get(["numberOfTabs"], (result) => {
+                resolve(result.numberOfTabs);
+            });
+        });
+    }
+    static setNumberOfTabs(numberOfTabs) {
+        chrome.storage.local.set({ numberOfTabs: numberOfTabs }, () => {
+            console.log("Settings saved");
+        });
+    }
+};
+
+const Badge = class {
+    static setBadgeText(numberOfTabs) {
+        chrome.browserAction.setBadgeText({ text: numberOfTabs.toString() });
+    }
+};
+
+async function main() {
+    chrome.tabs.query({ currentWindow: true }, async (tabs) => {
+        const NumberOfTabs = tabs.length;
+        const NumberOfTabsFromStorage = await StorageManager.getNumberOfTabs();
+        console.log(NumberOfTabsFromStorage);
     });
-}, 500);
+}
+main();
+// setInterval(main, 5500);
